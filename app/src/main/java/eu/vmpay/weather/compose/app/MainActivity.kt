0bef7ea1ade1bdcca539686e3c929a46ffd06841
andrii <andrii.iarkovoi@accenture.com>
2021-03-17 18:35:32 +0100

    Coil Image impl

--- app/src/main/java/eu/vmpay/weather/compose/app/MainActivity.kt
+++ app/src/main/java/eu/vmpay/weather/compose/app/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import dev.chrisbanes.accompanist.coil.CoilImage
 import eu.vmpay.weather.compose.app.models.ItemModel
 import eu.vmpay.weather.compose.app.ui.theme.WeatherComposeAppTheme
 import eu.vmpay.weather.compose.app.viewmodels.ListViewModel
@@ -42,42 +43,29 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun NewsStory() {
-    MaterialTheme {
-        val typography = MaterialTheme.typography
-        Column(modifier = Modifier.padding(16.dp)) {
-            Image(
-                painter = painterResource(id = R.drawable.header),
-                contentDescription = null,
-                modifier = Modifier
-                    .height(180.dp)
-                    .fillMaxWidth()
-                    .clip(shape = RoundedCornerShape(8.dp)),
-                contentScale = ContentScale.Crop
-            )
-            Spacer(Modifier.height(16.dp))
-            Text(
-                "A day wandering through the sandhills " +
-                        "in Shark Fin Cove, and a few of the " +
-                        "sights I saw",
-                style = typography.h6,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text("Davenport, California", style = typography.body2)
-            Text("December 2018", style = typography.body2)
-        }
-    }
-}
-
-@Composable
 fun ItemRow(item: ItemModel) {
     MaterialTheme {
         val typography = MaterialTheme.typography
         Column(modifier = Modifier.padding(16.dp)) {
-            Image(
-                painter = painterResource(id = R.drawable.header),
-                contentDescription = null,
+            CoilImage(
+                data = item.image,
+                contentDescription = item.title,
+                loading = {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        CircularProgressIndicator()
+                    }
+                },
+                error = {
+                    Image(
+                        painter = painterResource(id = R.drawable.header),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(180.dp)
+                            .fillMaxWidth()
+                            .clip(shape = RoundedCornerShape(8.dp)),
+                        contentScale = ContentScale.Crop
+                    )
+                },
                 modifier = Modifier
                     .height(180.dp)
                     .fillMaxWidth()
--- app/src/main/java/eu/vmpay/weather/compose/app/viewmodels/ListViewModel.kt
+++ app/src/main/java/eu/vmpay/weather/compose/app/viewmodels/ListViewModel.kt
@@ -6,43 +6,33 @@ import androidx.lifecycle.viewModelScope
 import eu.vmpay.weather.compose.app.models.ItemModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class ListViewModel : ViewModel() {
 
     val isLoading = MutableLiveData<Boolean>()
     val listLD = MutableLiveData<List<ItemModel>>()
 
-    private val list = listOf(
-        ItemModel(
-            "id1",
-            "A day wandering through the sandhills in Shark Fin Cove, and a few of the sights I saw",
-            "https://picsum.photos/300/300",
-            "Davenport, California",
-            1458220808_000
-        ),
-        ItemModel(
-            "id2",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            "https://picsum.photos/300/300",
-            "Davenport, California",
-            1455130808_000
-        ),
-        ItemModel(
-            "id3",
-            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "https://picsum.photos/300/300",
-            "Davenport, California",
-            1454130808_000
-        ),
-    )
-
     init {
-//        listLD.value = list
+        val newList = mutableListOf<ItemModel>()
+        val random = Random(9999)
+        for (i in 0 until 20) {
+            val id = random.nextInt(999)
+            newList.add(
+                ItemModel(
+                    "$id",
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    "https://picsum.photos/id/$id/1200/600",
+                    "Davenport, California $id",
+                    1454130808_000
+                )
+            )
+        }
         isLoading.value = true
         viewModelScope.launch {
-            delay(5_000)
+            delay(1_000)
             isLoading.value = false
-            listLD.value = list
+            listLD.value = newList
         }
     }
 }
